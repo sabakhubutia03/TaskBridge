64bec8fc191994cc29880c89a1092859ee223755
sabakhubutia03 <saba.khubutia03@geolab.edu.ge>
2026-04-11 13:07:16 +0400

    Initial project setup with Clean Architecture, added core entities (User, TaskItem, Application) and configured Swagger

---
+++ TaskBridge.API/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
