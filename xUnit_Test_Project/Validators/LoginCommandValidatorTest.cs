9e1c6e1cf321b2c2fd67ea358038408f3f51ed96
sabakhubutia03 <saba.khubutia03@geolab.edu.ge>
2026-06-11 15:00:58 +0400

    feat(TaskBridge): add unit tests for LoginCommandValidator and RegisterCommandValidator

---
+++ xUnit_Test_Project/Validators/LoginCommandValidatorTest.cs
@@ -0,0 +1,43 @@
+﻿using TaskBridge.Application.Commands;
+using TaskBridge.Application.Validatorss;
+
+namespace xUnit_Test_Project.Validators;
+
+public class LoginCommandValidatorTest
+{
+    private readonly LoginCommandValidator _validator;
+
+    public LoginCommandValidatorTest()
+    {
+        _validator = new LoginCommandValidator();
+    }
+
+    [Fact]
+    public async Task Login_WhenEmailInvalid_ShouldFail()
+    {
+        var login = new LoginCommand("notanemail", "password123");
+        
+       var result = await _validator.ValidateAsync(login);
+       Assert.False(result.IsValid);
+       Assert.Contains(result.Errors, e => e.PropertyName == "Email");
+    }
+
+    [Fact]
+    public async Task Login_WhenPasswordInvalid_ShouldFail()
+    {
+        var login = new LoginCommand("test@gmail.com", "123");
+        
+        var result = await _validator.ValidateAsync(login);
+        Assert.False(result.IsValid);
+        Assert.Contains(result.Errors, p => p.PropertyName == "Password");
+        
+    }
+
+    [Fact]
+    public async Task Login_WhenValid_ShouldPass()
+    {
+        var login = new LoginCommand("test@gmail.com", "Password123");
+        var result = await _validator.ValidateAsync(login);
+        Assert.True(result.IsValid);
+    }
+}
---
+++ xUnit_Test_Project/Validators/RegisterCommandValidatorTest.cs
@@ -0,0 +1,57 @@
+﻿using TaskBridge.Application.Commands;
+using TaskBridge.Application.Validatorss;
+
+namespace xUnit_Test_Project.Validators;
+
+public class RegisterCommandValidatorTest
+{
+    private readonly RegisterCommandValidator _validator;
+
+    public RegisterCommandValidatorTest()
+    {
+        _validator = new RegisterCommandValidator();
+    }
+
+    [Fact]
+    public async Task Register_WhenEmailNotValid_ReturnsFalse()
+    {
+        var register = new RegisterCommand(
+            "Email",
+            "Password123",
+            "FirsName",
+            "LastName");
+        
+        var result = await _validator.ValidateAsync(register);
+        Assert.False(result.IsValid);
+        Assert.Contains(result.Errors , e => e.PropertyName == "Email");
+    }
+
+    [Fact]
+    public async Task Register_WhenPasswordNotValid_ReturnsFalse()
+    {
+        var register = new RegisterCommand("email@gmail.com", "123", "FirsName", "LastName");
+        
+        var result = await _validator.ValidateAsync(register);
+        Assert.False(result.IsValid);
+        Assert.Contains(result.Errors , e => e.PropertyName == "Password");
+    }
+
+    [Fact]
+    public async Task Register_WhenFirstNameNotValid_ReturnsFalse()
+    {
+        var register = new RegisterCommand("email@gmail.com", "Password123", "", "LastName");
+        var result = await _validator.ValidateAsync(register);
+        Assert.False(result.IsValid);
+        Assert.Contains(result.Errors , e => e.PropertyName == "FirstName");
+    }
+
+    [Fact]
+    public async Task Register_WhenLastNameNotValid_ReturnsFalse()
+    {
+        var register = new RegisterCommand("email@gmail.com", "Password123", "FirsName", "");
+        var result = await _validator.ValidateAsync(register);
+        Assert.False(result.IsValid);
+        Assert.Contains(result.Errors , e => e.PropertyName == "LastName");
+    }
+    
+}
