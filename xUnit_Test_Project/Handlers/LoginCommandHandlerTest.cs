0eec9c16580996d1359ba69906e9ee75d934b9bb
sabakhubutia03 <saba.khubutia03@geolab.edu.ge>
2026-06-11 14:36:14 +0400

    feat(TaskBridge): add unit tests for LoginCommandHandler and RegisterCommandHandler

---
+++ xUnit_Test_Project/Handlers/LoginCommandHandlerTest.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using TaskBridge.Application.Commands;
+using TaskBridge.Domain.Entity;
+using TaskBridge.Domain.Enums;
+using TaskBridge.Infrastructure.Data;
+
+namespace xUnit_Test_Project.Handlers;
+
+public class LoginCommandHandlerTest
+{
+    private readonly AppDbContext _context;
+    private readonly Mock<IConfiguration> _configuration;
+    private readonly LoginCommandHendler _handler;
+
+    public LoginCommandHandlerTest()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString())
+            .Options;
+        _context = new AppDbContext(options);
+        
+        _configuration = new Mock<IConfiguration>();
+        _configuration
+            .Setup(x => x["JwtSettings:Key"])
+            .Returns("this_is_my_very_long_and_super_secret_key_64_characters_long_!!!");
+        
+        _handler = new LoginCommandHendler(
+            _context,
+            _configuration.Object
+        );
+    }
+
+
+    [Fact]
+    public async Task LoginCommandHandler_LoginCommand_Success()
+    {
+        var register = new RegisterCommand(
+            "Test@gmail.com",
+            "Password123",
+            "FirstNameTest",
+            "LastNameTest");
+
+        var user = new User
+        {
+            Id = Guid.NewGuid(),
+            Email = register.Email,
+            PasswordHash = BCrypt.Net.BCrypt.HashPassword(register.Password),
+            Role = Role.User,
+            CreatedAt = DateTime.Now
+        };
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+        
+        var login = new LoginCommand(register.Email, register.Password);
+        
+        var reuser = await _handler.Handle(login, CancellationToken.None);
+        Assert.NotNull(reuser);
+    }
+}
---
+++ xUnit_Test_Project/Handlers/RegisterCommandHandlerTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskBridge.Application.Commands;
+using TaskBridge.Infrastructure.Data;
+
+namespace xUnit_Test_Project.Handlers;
+
+public class RegisterCommandHandlerTest
+{
+    private readonly AppDbContext _context;
+    private readonly RegisterCommandHandler _handler;
+
+    public RegisterCommandHandlerTest()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString())
+            .Options;
+        _context = new AppDbContext(options);
+        
+        _handler = new RegisterCommandHandler(
+            _context
+        );
+    }
+
+    [Fact]
+    public async Task Register_WhenRegisterIsValid_ReturnsTrue()
+    {
+        var register = new RegisterCommand(
+            "Test@gmail.com",
+            "Password123",
+            "NameTest",
+            "LastNameTest");
+        ;
+        
+        var result = await _handler.Handle(register, CancellationToken.None);
+        Assert.NotNull(result);
+    }
+}
